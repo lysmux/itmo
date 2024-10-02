ea581872a62994fdf415b1495d0945da4343634c
lysmux <kllraz@yandex.ru>
2024-10-02 15:07:11 +0300

    feat(opd): add lab0

---
+++ opd/labs/lab0/task_5.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+cd lab0
+
+# Удалить файл conkeldurr6
+rm -v conkeldurr6
+
+# Удалить файл lab0/crawdaunt7/wormadam
+rm -fv crawdaunt7/wormadam
+
+# Удалить символические ссылки lab0/crawdaunt7/slakingcameru*
+chmod u+r crawdaunt7
+rm -v crawdaunt7/slakingcameru*
+
+# Удалить жесткие ссылки lab0/crawdaunt7/slakingconkeldu*
+rm -v crawdaunt7/slakingconkeldu*
+
+# Удалить директорию zorua3
+chmod -R u+w zorua3/
+rm -rv zorua3/
+
+# Удалить директорию lab0/parasect2/flaaffy
+rm -rv parasect2/flaaffy
